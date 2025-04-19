427af7b21a04d5bdd0f827d65e8a3aab40529ce1
rosa <rosahadi19@gmail.com>
2025-04-19 17:32:04 +0300

    feat: set up TypeORM, GraphQL (code-first), and global validation pipe

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { TypedConfigService } from './config/typed-config.service';
+import { typeOrmConfig } from './config/database.config';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: TypedConfigService) => ({
+        ...(await configService.get('database')),
+        entities: [User],
+      }),
+    }),
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      graphiql: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
