983a760de01f63bbf37e1761d227b7b02e822dd5
rosa <rosahadi19@gmail.com>
2025-04-20 22:04:08 +0300

    feat(server): enable CORS with dynamic client URL and add cookie parser

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import { FrontendConfig } from './config/frontend.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const clientUrl = configService.get<FrontendConfig>('frontend')?.clientUrl;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -11,6 +17,13 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: clientUrl,
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
