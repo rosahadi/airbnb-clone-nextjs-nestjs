3b51da219858e7ec5e02d99b16a10a050bc34ada
rosa <rosahadi19@gmail.com>
2025-05-04 22:05:21 +0300

    feat(client/graphql): add fragments, queries, and mutations for Property entity

---
+++ client/src/graphql/property.ts
@@ -0,0 +1,120 @@
+import { gql } from "@apollo/client";
+
+const PROPERTY_BASIC_FIELDS = gql`
+  fragment PropertyBasicFields on Property {
+    id
+    name
+    tagline
+    category
+    image
+    country
+    description
+    price
+    guests
+    bedrooms
+    beds
+    baths
+    amenities
+    createdAt
+    updatedAt
+  }
+`;
+
+const PROPERTY_WITH_USER_FIELDS = gql`
+  ${PROPERTY_BASIC_FIELDS}
+  fragment PropertyWithUserFields on Property {
+    ...PropertyBasicFields
+    user {
+      id
+      name
+      email
+    }
+  }
+`;
+
+const PROPERTY_WITH_RELATIONS_FIELDS = gql`
+  ${PROPERTY_WITH_USER_FIELDS}
+  fragment PropertyWithRelationsFields on Property {
+    ...PropertyWithUserFields
+    favorites {
+      id
+      userId
+    }
+    reviews {
+      id
+      rating
+      comment
+    }
+    bookings {
+      id
+      checkIn
+      checkOut
+    }
+  }
+`;
+
+// Queries
+export const GET_PROPERTIES_QUERY = gql`
+  ${PROPERTY_BASIC_FIELDS}
+  query GetProperties($filters: PropertyFilterInput) {
+    properties(filters: $filters) {
+      ...PropertyBasicFields
+    }
+  }
+`;
+
+export const GET_PROPERTY_QUERY = gql`
+  ${PROPERTY_WITH_RELATIONS_FIELDS}
+  query GetProperty($id: ID!) {
+    property(id: $id) {
+      ...PropertyWithRelationsFields
+    }
+  }
+`;
+
+export const GET_MY_PROPERTIES_QUERY = gql`
+  ${PROPERTY_WITH_USER_FIELDS}
+  query GetMyProperties {
+    myProperties {
+      ...PropertyWithUserFields
+    }
+  }
+`;
+
+// Mutations
+export const CREATE_PROPERTY_MUTATION = gql`
+  ${PROPERTY_WITH_USER_FIELDS}
+  mutation CreateProperty(
+    $createPropertyInput: CreatePropertyInput!
+  ) {
+    createProperty(
+      createPropertyInput: $createPropertyInput
+    ) {
+      ...PropertyWithUserFields
+    }
+  }
+`;
+
+export const UPDATE_PROPERTY_MUTATION = gql`
+  ${PROPERTY_WITH_USER_FIELDS}
+  mutation UpdateProperty(
+    $id: ID!
+    $updatePropertyInput: UpdatePropertyInput!
+  ) {
+    updateProperty(
+      id: $id
+      updatePropertyInput: $updatePropertyInput
+    ) {
+      ...PropertyWithUserFields
+    }
+  }
+`;
+
+export const DELETE_PROPERTY_MUTATION = gql`
+  ${PROPERTY_BASIC_FIELDS}
+  mutation DeleteProperty($id: ID!) {
+    deleteProperty(id: $id) {
+      ...PropertyBasicFields
+    }
+  }
+`;
